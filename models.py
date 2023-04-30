1c94fa97dcac7d78f7b68ad52bfd59a852260e82
p-almeida12 <p.almeida@MBP-de-Paulo.home>
2023-04-30 17:58:17 +0100

    pacient table and encryted password - ups esqueci me destes

---
+++ models.py
@@ -0,0 +1,10 @@
+
+class Pacient:
+    def __init__(self, id, first_name, last_name, care_taker, doctor_id, birth_date, doctor_name):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.care_taker = care_taker
+        self.doctor_id = doctor_id
+        self.birth_date = birth_date
+        self.doctor_name = doctor_name
